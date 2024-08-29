750f131d02d903d36d6d08dadfab7e0d307e570f
reosato <rsato@iplab.cs.tsukuba.ac.jp>
2024-08-30 02:53:59 +0900

    add theme.css can change app theme

--- Busshi/src/main.js
+++ Busshi/src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'  // Vuex ストアのインポート
+// import './assets/theme.css'; // テーマの変更に対応させるため（必要ない場合はコメントアウト）
 
 const app = createApp(App);  // アプリケーションインスタンスを作成
 app.use(store);              // Vuex ストアをアプリケーションに登録
